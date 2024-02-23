6b9f7fd3b59b0a64f43ec11f349a2803638baf2c
Pidopryhora Vitaliy <vpidopryhora@gmail.com>
2024-02-23 17:09:18 +0200

    createReducer (206)

--- src/actions/index.js
+++ src/actions/index.js
@@ -20,55 +20,67 @@ export const heroesFetched = createAction('HEROES_FETCHED');
 //         payload: heroes
 //     }
 // }
-export const heroesFetchingError = () => {
-    return {
-        type: 'HEROES_FETCHING_ERROR'
-    }
-}
+export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR');
+// export const heroesFetchingError = () => {
+//     return {
+//         type: 'HEROES_FETCHING_ERROR'
+//     }
+// }
 
-export const heroesDeleting = () => {
-    return {
-        type: 'HEROES_DELETING'
-    }
-}
-export const heroesDeleted = (heroesId) => {
-    return {
-        type: 'HEROES_DELETED',
-        payload: heroesId
-    }
-}
-export const heroesDeletingError = () => {
-    return {
-        type: 'HEROES_DELETING_ERROR'
-    }
-}
+export const heroesDeleting = createAction('HEROES_DELETING');
+// export const heroesDeleting = () => {
+//     return {
+//         type: 'HEROES_DELETING'
+//     }
+// }
+export const heroesDeleted = createAction('HEROES_DELETED');
+// export const heroesDeleted = (heroesId) => {
+//     return {
+//         type: 'HEROES_DELETED',
+//         payload: heroesId
+//     }
+// }
+export const heroesDeletingError = createAction('HEROES_DELETING_ERROR');
+// export const heroesDeletingError = () => {
+//     return {
+//         type: 'HEROES_DELETING_ERROR'
+//     }
+// }
 
-export const heroesAdding = () => {
-    return {
-        type: 'HEROES_ADDING'
-    }
-}
-export const heroesAdded = (heroes) => {
-    return {
-        type: 'HEROES_ADDED',
-        payload: heroes
-    }
-}
-export const heroesAddingError = () => {
-    return {
-        type: 'HEROES_ADDING_ERROR'
-    }
-}
-export const heroesAddingErrorData = () => {
-    return {
-        type: 'HEROES_ADDING_ERROR_DATA'
-    }
-}
-export const heroesAddingClearError = () => {
-    return {
-        type: 'HEROES_ADDING_CLEAR_ERROR'
-    }
-}
+export const heroesAdding = createAction('HEROES_ADDING');
+// export const heroesAdding = () => {
+//     return {
+//         type: 'HEROES_ADDING'
+//     }
+// }
+export const heroesAdded = createAction('HEROES_ADDED');
+// export const heroesAdded = (heroes) => {
+//     return {
+//         type: 'HEROES_ADDED',
+//         payload: heroes
+//     }
+// }
+export const heroesAddingError = createAction('HEROES_ADDING_ERROR');
+// export const heroesAddingError = () => {
+//     return {
+//         type: 'HEROES_ADDING_ERROR'
+//     }
+// }
+export const heroesAddingErrorData = createAction('HEROES_ADDING_ERROR_DATA');
+// export const heroesAddingErrorData = () => {
+//     return {
+//         type: 'HEROES_ADDING_ERROR_DATA'
+//     }
+// }
+export const heroesAddingClearError = createAction('HEROES_ADDING_CLEAR_ERROR');
+// export const heroesAddingClearError = () => {
+//     return {
+//         type: 'HEROES_ADDING_CLEAR_ERROR'
+//     }
+// }
+
+
+// FILTERS
 
 export const fetchFilters = (request) => (dispatch) => {
     dispatch(filtersFetching());
--- src/reducers/heroes.js
+++ src/reducers/heroes.js
@@ -1,3 +1,10 @@
+import { createReducer } from "@reduxjs/toolkit";
+import {
+    heroesFetched, heroesFetching, heroesFetchingError,
+    heroesAdding, heroesAdded, heroesAddingError, heroesAddingErrorData,
+    heroesDeleted, heroesDeleting, heroesDeletingError
+} from "../actions";
+
 const initialState = {
     heroes: [],
     heroesLoadingStatus: 'idle',
@@ -5,71 +12,144 @@ const initialState = {
     heroesAddingStatus: 'idle',
 }
 
-const heroes = (state = initialState, action) => {
-    switch (action.type) {
-        case 'HEROES_FETCHING':
-            return {
-                ...state,
-                heroesLoadingStatus: 'loading'
-            }
-        case 'HEROES_FETCHED':
-            return {
-                ...state,
-                heroes: action.payload,
-                heroesLoadingStatus: 'idle'
-            }
-        case 'HEROES_FETCHING_ERROR':
-            return {
-                ...state,
-                heroesLoadingStatus: 'error'
-            }
+const heroes = createReducer(initialState, {
+    // FETCH
+    [heroesFetching]: state => { state.heroesLoadingStatus = 'loading' },
+    [heroesFetched]: (state, action) => {
+        state.heroesLoadingStatus = 'idle';
+        state.heroes = action.payload;
+    },
+    [heroesFetchingError]: state => { state.heroesLoadingStatus = 'error' },
+    // ADD
+    [heroesAdding]: state => { state.heroesAddingStatus = 'loading' },
+    [heroesAdded]: (state, action) => {
+        state.heroesAddingStatus = 'idle';
+        state.heroes.push(action.payload);
+    },
+    [heroesAddingError]: state => { state.heroesAddingStatus = 'error'; },
+    [heroesAddingErrorData]: state => { state.heroesAddingStatus = 'error_data'; },
+    // DELETE
+    [heroesDeleting]: state => { state.heroesDeletingStatus = 'loading' },
+    [heroesDeleted]: (state, action) => {
+        state.heroesDeletingStatus = 'idle';
+        state.heroes = state.heroes.filter(item => item.id !== action.payload);
+    },
+    [heroesDeletingError]: state => { state.heroesDeletingStatus = 'error' },
+},
+    [],
+    // DEFAULT
+    state => state
+);
 
-        case 'HEROES_DELETING':
-            return {
-                ...state,
-                heroesDeletingStatus: 'loading'
-            }
-        case 'HEROES_DELETED':
-            return {
-                ...state,
-                heroes: state.heroes.filter(item => item.id !== action.payload),
-                heroesDeletingStatus: 'idle'
-            }
-        case 'HEROES_DELETING_ERROR':
-            return {
-                ...state,
-                heroesDeletingStatus: 'error'
-            }
+// const heroes = createReducer(initialState, builder => {
+//     builder
+//         // FETCH
+//         .addCase(heroesFetching, state => {
+//             state.heroesLoadingStatus = 'loading'
+//         })
+//         .addCase(heroesFetched, (state, action) => {
+//             state.heroesLoadingStatus = 'idle';
+//             state.heroes = action.payload;
+//         })
+//         .addCase(heroesFetchingError, state => {
+//             return {
+//                 ...state,
+//                 heroesLoadingStatus: 'error'
+//             }
+//         })
+//         // ADD
+//         .addCase(heroesAdding, state => {
+//             state.heroesAddingStatus = 'loading';
+//         })
+//         .addCase(heroesAdded, (state, action) => {
+//             state.heroesAddingStatus = 'idle';
+//             state.heroes.push(action.payload);
+//         })
+//         .addCase(heroesAddingError, state => {
+//             state.heroesAddingStatus = 'error';
+//         })
+//         .addCase(heroesAddingErrorData, state => {
+//             state.heroesAddingStatus = 'error_data';
+//         })
+//         // DELETE
+//         .addCase(heroesDeleting, state => {
+//             state.heroesDeletingStatus = 'loading'
+//         })
+//         .addCase(heroesDeleted, (state, action) => {
+//             state.heroesDeletingStatus = 'idle';
+//             state.heroes = state.heroes.filter(item => item.id !== action.payload);
+//         })
+//         .addCase(heroesDeletingError, state => {
+//             state.heroesDeletingStatus = 'error';
+//         })
+//         // DEFAULT
+//         .addDefaultCase(() => { });
+// });
 
-        case 'HEROES_ADDING':
-            return {
-                ...state,
-                heroesAddingStatus: 'loading'
-            }
-        case 'HEROES_ADDED':
-            return {
-                ...state,
-                heroes: [...state.heroes, action.payload],
-                heroesAddingStatus: 'idle'
-            }
-        case 'HEROES_ADDING_ERROR':
-            return {
-                ...state,
-                heroesAddingStatus: 'error'
-            }
-        case 'HEROES_ADDING_ERROR_DATA':
-            return {
-                ...state,
-                heroesAddingStatus: 'error_data'
-            }
-        case 'HEROES_ADDING_CLEAR_ERROR':
-            return {
-                ...state,
-                heroesAddingStatus: 'idle'
-            }
+// const heroes = (state = initialState, action) => {
+//     switch (action.type) {
+//         case 'HEROES_FETCHING':
+//             return {
+//                 ...state,
+//                 heroesLoadingStatus: 'loading'
+//             }
+//         case 'HEROES_FETCHED':
+//             return {
+//                 ...state,
+//                 heroes: action.payload,
+//                 heroesLoadingStatus: 'idle'
+//             }
+//         case 'HEROES_FETCHING_ERROR':
+//             return {
+//                 ...state,
+//                 heroesLoadingStatus: 'error'
+//             }
 
-        default: return state
-    }
-}
+//         case 'HEROES_DELETING':
+//             return {
+//                 ...state,
+//                 heroesDeletingStatus: 'loading'
+//             }
+//         case 'HEROES_DELETED':
+//             return {
+//                 ...state,
+//                 heroes: state.heroes.filter(item => item.id !== action.payload),
+//                 heroesDeletingStatus: 'idle'
+//             }
+//         case 'HEROES_DELETING_ERROR':
+//             return {
+//                 ...state,
+//                 heroesDeletingStatus: 'error'
+//             }
+
+//         case 'HEROES_ADDING':
+//             return {
+//                 ...state,
+//                 heroesAddingStatus: 'loading'
+//             }
+//         case 'HEROES_ADDED':
+//             return {
+//                 ...state,
+//                 heroes: [...state.heroes, action.payload],
+//                 heroesAddingStatus: 'idle'
+//             }
+//         case 'HEROES_ADDING_ERROR':
+//             return {
+//                 ...state,
+//                 heroesAddingStatus: 'error'
+//             }
+//         case 'HEROES_ADDING_ERROR_DATA':
+//             return {
+//                 ...state,
+//                 heroesAddingStatus: 'error_data'
+//             }
+//         case 'HEROES_ADDING_CLEAR_ERROR':
+//             return {
+//                 ...state,
+//                 heroesAddingStatus: 'idle'
+//             }
+//         default: return state
+//     }
+// }
 
 export default heroes;
