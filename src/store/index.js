2a73d80148d5c97ca1aecbcfe76582784f946e0e
Pidopryhora Vitaliy <vpidopryhora@gmail.com>
2024-02-23 10:21:25 +0200

    Middleware (202)

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,47 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
 // import reducer from '../reducers';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const enhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
-    const oldDispatch = store.dispatch;
-    store.dispatch = (action) => {
-        if (typeof action === 'string') {
-            return oldDispatch({ type: action })
-        }
-        return oldDispatch(action)
+const stringMiddleware = (store) => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next({ type: action });
     }
-    return store;
-}
+    return next(action);
+};
 
-// const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(
+    combineReducers({ heroes, filters }),
+    compose(
+        applyMiddleware(stringMiddleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
+);
+
+// const enhancer = (createStore) => (...args) => {
+//     const store = createStore(...args);
+//     const oldDispatch = store.dispatch;
+//     store.dispatch = (action) => {
+//         if (typeof action === 'string') {
+//             return oldDispatch({ type: action })
+//         }
+//         return oldDispatch(action)
+//     }
+//     return store;
+// }
+// const store = createStore(
+//     combineReducers({ heroes, filters }),
+//     compose(
+//         enhancer,
+//         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//     )
+// );
+
 // const store = createStore(
 //     combineReducers({ heroes, filters }),
 //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 // );
-const store = createStore(
-    combineReducers({ heroes, filters }),
-    compose(
-        enhancer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
-);
+
+// const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 export default store;
